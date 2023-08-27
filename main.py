c1c32387b92d7698ce5854a939aad4cec63455f3
Maximilien Garcia <niwamaxou@gmail.com>
2023-08-27 17:19:26 +0800

    [PICOSSA] feat: first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+import requests
+import os
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(intents = intents, command_prefix=commands.when_mentioned)
+openai_token = os.getenv('OPENAI_TOKEN')
+picossa_token = os.getenv('PICOSSA_TOKEN')
+
+headers = {"Authorization": "Bearer " + openai_token}
+
+@bot.event
+async def on_ready():
+    print(f"Successfully logged in as {bot.user.name}!")
+
+@bot.event
+async def on_message(message):
+    if message.author.bot == False and bot.user.mentioned_in(message):
+        prompt = message.clean_content.replace('@Picossa', '')
+        print(prompt)
+        global free_to_generate
+        new_data = {"prompt": prompt,"n": 1,"size": "512x512","response_format": "url"}
+        print(new_data)        
+        post_response = requests.post("https://api.openai.com/v1/images/generations", json=new_data, headers=headers)
+        post_response_json = post_response.json()
+        print(post_response_json)
+        if (post_response_json["error"]):
+            await message.channel.send("prompt interdit car : [" + post_response_json["error"]["message"] + "]")
+            return
+        await message.channel.send(post_response_json["data"][0]["url"])
+        
+bot.run(picossa_token)
