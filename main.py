df3cffea7c5fcf7b330d8fdf9af78efcef57454f
Maximilien Garcia <niwamaxou@gmail.com>
2023-11-10 18:48:03 +0800

    PICOSSA (feat): adding translation in french

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if message.author.bot == False and bot.user.mentioned_in(message):
+        userToReply = message.author
         prompt = message.clean_content.replace('@Picossa', '')
         print(f'{"prompt : "}{"["}{prompt}{"]"}')
         new_data = {"prompt": prompt,"n": 1,"size": "1792x1024","response_format": "url", "model": "dall-e-3", "quality": "hd"}
@@ -31,7 +32,7 @@ async def on_message(message):
                 fp = open('image01.webp', 'wb')
                 fp.write(response.content)
                 fp.close()
-                await message.channel.send(revisedPrompt, file=discord.File('image01.webp'))
+                await message.channel.send(f'{userToReply.mention} : {await translateInFrench(revisedPrompt)}', file=discord.File('image01.webp'))
                 return
             await message.channel.send("J'ai que l'url pour cette image : " + post_response_json["data"][0]["url"])
             return
@@ -41,3 +42,23 @@ async def on_message(message):
     await bot.process_commands(message)
 
 bot.run(picossa_token)
+
+def translateInFrench(message):
+    body = """{
+        "model": "gpt-3.5-turbo",
+        "messages": [
+        {
+            "role": "system",
+            "content": "Your only goal is to translate from english to french the messages you'll recceive as input"
+        },
+        {
+            "role": "user",
+            "content": {message}
+        }
+        ]
+    }"""
+    post_response = requests.post("https://api.openai.com/v1/images/generations", json=body, headers=headers)
+    post_response_json = post_response.json()
+    if (post_response_json.get("error") == None):
+        translatedMessage = post_response_json["choices"][0]["message"]["content"]
+    return translatedMessage
